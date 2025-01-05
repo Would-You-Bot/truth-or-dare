dde57da4c710f68f44f49ac7270aa1f0f9131506
Dominik Koch <dominik@koch-bautechnik.de>
2025-01-05 02:18:02 +0100

    feat(seo): add sitemap

---
+++ apps/web/app/sitemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://truthordare.gg",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://truthordare.gg/features",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://truthordare.gg/about",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://truthordare.gg/privacy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
+    {
+      url: "https://truthordare.gg/legal",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://truthordare.gg/terms",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+  ];
+}
