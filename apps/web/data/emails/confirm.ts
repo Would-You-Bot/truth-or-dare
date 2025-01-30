7f9199b6a9ab620d11f14e2dc8f1f96c69150f3d
Dominik Koch <dominik@koch-bautechnik.de>
2025-01-30 08:28:30 +0000

    fix: add our email

--- apps/web/data/emails/confirm.ts
+++ apps/web/data/emails/confirm.ts
@@ -1,6 +1,6 @@
 import type { Email } from '@/types/emails'
 
 export const confirmEmailData: Email = {
-  from: 'Truth Or Dare <onboarding@resend.dev>',
+  from: 'Truth Or Dare <noreply@truthordare.gg>',
   subject: 'Confirm your email address'
 }
