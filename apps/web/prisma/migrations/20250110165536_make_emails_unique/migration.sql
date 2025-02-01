1381b2637892fb54a3ac9643e98262f8fbf9841d
Dominik Koch <dominik@koch-bautechnik.de>
2025-02-01 23:19:47 +0100

    delete old migrations

--- apps/web/prisma/migrations/20250110165536_make_emails_unique/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[email]` on the table `Waitlist` will be added. If there are existing duplicate values, this will fail.
-
-*/
-
--- CreateIndex
-CREATE UNIQUE INDEX "Waitlist_email_key" ON "Waitlist"("email");
