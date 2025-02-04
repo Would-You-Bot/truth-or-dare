3f219174e176c9d0e5bb525749d7aa356b0da915
Dominik Koch <dominik@koch-bautechnik.de>
2025-02-04 18:48:31 +0000

    fix: move server stuff to microservice

--- apps/web/server/jobs.ts
+++
@@ -1,8 +0,0 @@
-import cron from 'node-cron'
-import { deleteUnconfirmedUsers } from './tasks/deleteUnconfirmed'
-import { EVERY_MINUTE } from './utils'
-
-export const deleteUnconfirmedCronJob = cron.schedule(
-  EVERY_MINUTE,
-  deleteUnconfirmedUsers
-)
--- apps/web/server/utils.ts
+++
@@ -1,8 +0,0 @@
-// Predefined cron job times
-export const EVERY_MINUTE = '* * * * *'
-export const EVERY_FIVE_MINUTES = '*/5 * * * *'
-export const EVERY_HOUR = '0 * * * *'
-export const EVERY_DAY = '0 0 * * *'
-export const EVERY_WEEK = '0 0 * * 0'
-export const EVERY_MONTH = '0 0 1 * *'
-export const EVERY_YEAR = '0 0 1 1 *'
