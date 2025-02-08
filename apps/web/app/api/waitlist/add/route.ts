8a3a958de46ddfa5b13da696fabdcfe395a0b16a
Dominik Koch <dominik@koch-bautechnik.de>
2025-02-08 20:11:35 +0100

    fix: make prisma work again

--- apps/web/app/api/waitlist/add/route.ts
+++ apps/web/app/api/waitlist/add/route.ts
@@ -24,8 +24,6 @@ export async function POST(req: Request) {
         headers: { "Content-Type": "application/json" },
       });
     }
-
-    // Zod email validation
     
     const { success } = EmailSchema.safeParse(body);
 
